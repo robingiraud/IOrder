64bf8fde04a5eb848e764eda394b088486263d3a
Robin Giraud <robin.giraud5@gmail.com>
2021-04-16 12:50:25 +0200

    Register page

---
+++ src/views/auth/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form class="login" @submit.prevent="login">
+      <h1>Connectez-vous</h1>
+      <div style="display: flex; flex-direction: column">
+        <span v-if="authStatus === 'error'">Ces identifiants n'existent pas!</span>
+        <input style="margin-top: .6rem" id="emai" required v-model="email" type="email" placeholder="Email"/>
+        <input style="margin-top: .6rem" id="password" required v-model="password" type="password" placeholder="Mot de passe"/>
+      </div>
+      <button style="margin-top: .6rem" type="submit">Connexion</button>
+      <div style="font-size: .6em; margin-top: .6rem">Vous n'êtes pas encore inscrit ? <u>Créer un compte</u></div>
+    </form>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "Login.vue",
+  computed: {
+    ...mapGetters({
+      authStatus: 'auth/authStatus'
+    })
+  },
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: '',
+    }
+  },
+  methods: {
+    login: function () {
+      const { email, password } = this
+      this.$store.dispatch('auth/AUTH_REQUEST', { email, password }).then(() => {
+        if (this.authStatus !== 'error') {
+          this.$router.push('/')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
