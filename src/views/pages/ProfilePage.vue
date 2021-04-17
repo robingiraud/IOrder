e64aeb1768a32e3ccf1579642b95e569aade2cc0
Robin Giraud <robin.giraud5@gmail.com>
2021-04-17 16:52:33 +0200

    :sparkles: Logout

--- src/views/pages/ProfilePage.vue
+++ src/views/pages/ProfilePage.vue
@@ -4,13 +4,13 @@
       <li>
         <i class="bx bxs-user-account bx-sm" />
         <div>
-          <h3>Robin Giraud</h3>
+          <h3>{{ user.name }}</h3>
           <span>Voir mon profil</span>
         </div>
       </li>
       <li>
         <i class="bx bxs-bell-ring bx-sm" />
-        <h3>Notifications</h3>
+        <h3>Notifications [WIP]</h3>
       </li>
       <li>
         <i class="bx bxs-wrench bx-sm" />
@@ -20,6 +20,10 @@
         <i class="bx bx-donate-heart bx-sm" />
         <h3>Faire un don</h3>
       </li>
+      <li style="color: lightcoral" @click="logout">
+        <i class="bx bx-log-out bx-sm" />
+        <h3>Se déconnecter</h3>
+      </li>
     </ul>
     <!-- Mon profil
     <button @click="logout">Déconnexion</button> -->
@@ -27,7 +31,14 @@
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
+  computed: {
+    ...mapGetters({
+      user: 'auth/user'
+    })
+  },
   methods: {
     logout: function () {
       this.$store.dispatch('auth/AUTH_LOGOUT').then(() => {
